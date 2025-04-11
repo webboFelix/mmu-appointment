b049fe71692c47e9733486950ed0940d50bf0ea0
Admin <felixwebbo8@gmail.com>
2025-04-11 20:07:04 +0300

    modified next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Disable minifier if it's causing issues
+  swcMinify: false,
+  // Or use:
+  // optimization: { minimize: false }
 };
 
 export default nextConfig;
